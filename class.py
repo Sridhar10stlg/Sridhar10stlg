3c4bf8bda9458e767485ae09872bcb3f1f77fb03
Sridhar10stlg <74764419+Sridhar10stlg@users.noreply.github.com>
2022-08-14 13:49:43 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+
+class Player:
+    fees = 1000
+    def __init__(self, name, age, role):
+        self.name = name
+        self.role = role
+        self.age = age
+
+    def address(self):
+        add = f" Name:{self.name}\n Age : {self.age}\n Role: {self.role}\n Fees:" 
+        return add  
+    
+    
+
+P1 = Player('Hari', 20, 'Fast Bowler')
+P2 = Player('Arun', 20, 'Batsman')
+
+print(P1.address())
+print(P1.fees)
